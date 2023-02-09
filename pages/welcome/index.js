dd65f075ed14cee4091a18734fb5b77be0773528
“NilsNeumann11” <nilstorbenneumann@gmail.com>
2023-02-09 17:17:06 +0100

    feat: add welcome page

--- pages/welcome/index.js
+++ pages/welcome/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import LargeBtn from "@/components/largeBtn/largeBtn";
+import styles from "./welcome.module.css";
 
 const WelcomePage = () => {
 	return (
-		<div>
-			<h1>WelcomePage</h1>
+		<div className={styles.welcome}>
+			<h1>Silent Moon</h1>
+			<h2>Hi Leon, welcome to Silent Moon</h2>
+			<Image
+				src={"/img/welcome.png"}
+				width="415"
+				height="699"
+				alt="woman does yoga"
+			/>
+			<LargeBtn url="/reminders">Get Started</LargeBtn>
 		</div>
 	);
 };
