58793272b0c236d1571893355c3f3ba3ee50df31
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-09 13:57:54 +0100

    styling: add phone frame

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import Link from "next/link";
+import { Roboto } from "@next/font/google";
+import "@/styles/globals.css";
+import styles from "./app.module.css";
+
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div className={`${styles.smartphone} ${roboto.className}`}>
+			<div className={styles.content}>
+				<Component {...pageProps} />
+			</div>
+			<Link href={"/"} className={styles.homeBtn}></Link>
+		</div>
+	);
 }
