845f9759326963e22df52d0f43b8fc70cc73e3d1
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-09 20:56:30 +0100

    styling: add styling of reminders page without time carrousel

--- pages/reminders/index.js
+++ pages/reminders/index.js
@@ -1,9 +1,32 @@
+import LargeBtn from "@/components/largeBtn/largeBtn";
 import React from "react";
+import Link from "next/link";
+import styles from "./reminders.module.css";
 
 const RemindersPage = () => {
 	return (
-		<div>
-			<h1>RemindersPage</h1>
+		<div className={styles.reminders}>
+			<h1>Silent Moon</h1>
+			<h2>What time would you like to meditate?</h2>
+			<p>
+				You can meditate at any time, but we recommend to do in the morning.
+			</p>
+			<div className={styles.timeContainer}></div>
+			<h2>On which days would you like to meditate?</h2>
+			<p>Everyday is best, but we recommend picking at least five days.</p>
+			<div className={styles.dayContainer}>
+				<div className={styles.day}>SU</div>
+				<div className={styles.day}>M</div>
+				<div className={styles.day}>TU</div>
+				<div className={styles.day}>W</div>
+				<div className={styles.day}>TH</div>
+				<div className={styles.day}>F</div>
+				<div className={styles.day}>SA</div>
+			</div>
+			<LargeBtn url="/home">SAVE</LargeBtn>
+			<Link href="/home" className={styles.noThanks}>
+				NO THANKS
+			</Link>
 		</div>
 	);
 };
