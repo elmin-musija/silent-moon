1f2cefd2f326a06564d5bd255955175525b8be4e
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-09 15:08:09 +0100

    styling: change font family to source sans pro

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import Link from "next/link";
-import { Roboto } from "@next/font/google";
+import { Source_Sans_Pro } from "@next/font/google";
 import "@/styles/globals.css";
 import styles from "./app.module.css";
 
-const roboto = Roboto({ subsets: ["latin"], weight: "400" });
+const sourceSansPro = Source_Sans_Pro({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }) {
 	return (
-		<div className={`${styles.smartphone} ${roboto.className}`}>
+		<div className={`${styles.smartphone} ${sourceSansPro.className}`}>
 			<div className={styles.content}>
 				<Component {...pageProps} />
 			</div>
-			<Link href={"/"} className={styles.homeBtn}></Link>
+			<Link href="/" className={styles.homeBtn}></Link>
 		</div>
 	);
 }
