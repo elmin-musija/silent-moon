39407ab1f7250882105405fa5cc8abf4ed672dc3
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-12 16:51:30 +0100

    feat: add remote patterns for playlist images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.scdn.co",
+				port: "",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "mosaic.scdn.co",
+				port: "",
+				pathname: "**",
+			},
+		],
+	},
+};
