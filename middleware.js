df4496667d56259ffca8876b54876c12d600d8ba
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-13 10:33:44 +0100

    feat: add middleware with protected routes to project

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req) {
+	// const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+	const session = await getToken({ req });
+	if (!session) {
+		return NextResponse.redirect(new URL("/signin", req.url));
+	}
+	console.log({ session });
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: [
+		"/welcome/:path",
+		"/reminders/:path",
+		"/yoga/:path",
+		"/meditate/:path",
+		"/home/:path",
+		"/music/:path",
+		"/profile/:path",
+	],
+};
