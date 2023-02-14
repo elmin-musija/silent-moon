e8e5d1ed1ab2d19dc662c40e9185f2fb17a474c1
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-14 13:30:09 +0100

    feat: add function to list all categories to use cases

--- src/services/use-cases/index.js
+++ src/services/use-cases/index.js
@@ -1,9 +1,11 @@
 import { insertYogaExercise } from "@/src/services/use-cases/insertYogaExercise";
 import { listAllYogaExercises } from "@/src/services/use-cases/listAllYogaExercises";
+import { listAllYogaCategories } from "@/src/services/use-cases/listAllYogaCategories";
 
 const YogaService = {
 	insertYogaExercise,
 	listAllYogaExercises,
+	listAllYogaCategories,
 };
 
 module.exports = {
