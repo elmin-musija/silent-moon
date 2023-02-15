66cdb6287679664c0f8029b30cf98e24c3704511
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-15 16:07:50 +0100

    feat: add user and meditation model to project

---
+++ src/models/UserModel.js
@@ -0,0 +1,16 @@
+import { mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	// _id: { type: mongoose.Types.ObjectId },
+	name: {
+		firstname: { type: String, required: true },
+		lastname: { type: String, required: true },
+	},
+	email: { type: String },
+	favorites: {
+		yoga: [{ type: mongoose.Types.ObjectId, ref: "Yoga" }],
+		meditation: [{ type: mongoose.Types.ObjectId, ref: "Mediation" }],
+	},
+});
+
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
