11dae03d2a55c8d6ffae5f7ab1aca5ab1903bac6
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-15 16:13:07 +0100

    styling: add styling of yoga page

--- pages/yoga/index.js
+++ pages/yoga/index.js
@@ -4,6 +4,7 @@ import { uid } from "uid";
 import Image from "next/image";
 import Link from "next/link";
 import { YogaService } from "@/src/services/use-cases/index";
+import styles from "./yoga.module.css";
 
 const YogaPage = ({ yogaPrograms, yogaCategories }) => {
 	const inputFieldSearchRef = useRef(null);
@@ -39,45 +40,60 @@ const YogaPage = ({ yogaPrograms, yogaCategories }) => {
 	}, [categoryFilter]);
 
 	return (
-		<div>
+		<div className={styles.yogaPage}>
 			<Title />
 			<h2>Yoga</h2>
 			<p>Find your innter zen from anywhere</p>
 
 			{/** Categories*/}
-			{yogaCategories.map((element) => (
-				<button key={uid()} onClick={() => setCategoryFilter(element._id)}>
-					{element._id}
-				</button>
-			))}
+			<div className={styles.categorySlider}>
+				{yogaCategories.map((element) => (
+					<button key={uid()} onClick={() => setCategoryFilter(element._id)}>
+						<Image
+							src={`/img/${element._id.toLowerCase()}.svg`}
+							width="65"
+							height="65"
+							alt={`${element._id} icon`}
+						/>
+						<p className={styles.categoryName}>{element._id}</p>
+					</button>
+				))}
+			</div>
 
 			{/** Search bar */}
-			<form onChange={onInputSearchYogaHandler}>
-				<input
-					type="text"
-					name="input-yoga-search"
-					id="input-yoga-search"
-					ref={inputFieldSearchRef}
-					placeholder="Search for yoga session"
-				/>
-			</form>
+			<div className={styles.searchbar}>
+				<form onChange={onInputSearchYogaHandler}>
+					<input
+						type="text"
+						name="input-yoga-search"
+						id="input-yoga-search"
+						ref={inputFieldSearchRef}
+						placeholder="Search for yoga session"
+					/>
+				</form>
+				<div className={styles.searchIconContainer}>
+					<Image src="/img/search.svg" width="15" height="15" alt="search" />
+				</div>
+			</div>
 
 			{/** Music player */}
 
 			{/** Yoga */}
-			{filteredYogaPrograms?.map((element) => (
-				<Link key={element._id} href={`/yoga/${element._id}`}>
-					<Image
-						src={element.imageUrl}
-						width={200}
-						height={200}
-						alt={element.title}
-						key={element._id}
-						priority
-					></Image>
-					<h3>{element.title}</h3>
-				</Link>
-			))}
+			<div className={styles.yogaGallery}>
+				{filteredYogaPrograms?.map((element) => (
+					<Link key={element._id} href={`/yoga/${element._id}`}>
+						<Image
+							src={element.imageUrl}
+							width={200}
+							height={200}
+							alt={element.title}
+							key={element._id}
+							priority
+						></Image>
+						<h3>{element.title}</h3>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 };
