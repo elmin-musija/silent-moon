c548bd588bb634cdb7ba9c75486f39149ee5a998
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-16 12:30:43 +0100

    styling: add styling with sliders of profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,30 +3,77 @@ import React from "react";
 import { getServerSession } from "next-auth/next";
 import { NextAuthOptions } from "../api/auth/[...nextauth]";
 import { UserService } from "@/src/services/use-cases/index";
+import { uid } from "uid";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./profile.module.css";
 
 const ProfilePage = ({ allYogaFavorites }) => {
 	return (
-		<div>
+		<div className={styles.profilePage}>
 			<Title />
-			<h2>Profile</h2>
-			<div className={styles.yogaGallery}>
+			<h2>Favourite Yoga Session</h2>
+			<div className={styles.slider}>
 				{allYogaFavorites?.map((element) => (
-					<Link key={element._id} href={`/yoga/${element._id}`}>
-						<Image
-							src={element.imageUrl}
-							width={200}
-							height={200}
-							alt={element.title}
-							key={element._id}
-							priority
-						></Image>
-						<h3>{element.title}</h3>
+					<Link
+						key={element._id}
+						href={`/yoga/${element._id}`}
+						className={styles.sliderItem}
+					>
+						<div className={styles.imgageContainer} key={uid()}>
+							<Image
+								src={element.imageUrl}
+								width={150}
+								height={150}
+								alt={element.title}
+								key={element._id}
+								priority
+							></Image>
+						</div>
+
+						<div key={uid()} className={styles.itemInfo}>
+							<h3>{element.title}</h3>
+							<div className={styles.itemSubInfo}>
+								<p key={uid()}>{element.level}</p>
+								<p key={uid()}>
+									{element.duration.minutes}:{element.duration.seconds} min
+								</p>
+							</div>
+						</div>
 					</Link>
 				))}
 			</div>
+			<h2>Favourite Meditations</h2>
+			{/* <div className={styles.slider}>
+				{allMeditationFavorites?.map((element) => (
+					<Link
+						key={element._id}
+						href={`/yoga/${element._id}`}
+						className={styles.sliderItem}
+					>
+						<div className={styles.imgageContainer} key={uid()}>
+							<Image
+								src={element.imageUrl}
+								width={150}
+								height={150}
+								alt={element.title}
+								key={element._id}
+								priority
+							></Image>
+						</div>
+
+						<div key={uid()} className={styles.itemInfo}>
+							<h3>{element.title}</h3>
+							<div className={styles.itemSubInfo}>
+								<p key={uid()}>{element.level}</p>
+								<p key={uid()}>
+									{element.duration.minutes}:{element.duration.seconds} min
+								</p>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div> */}
 		</div>
 	);
 };
