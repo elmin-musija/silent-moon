db42b3d863bb2d1b12cc707f8e36fef0f1b47176
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-16 09:50:14 +0100

    feat: add function to add yoga exercises to user favorites in use cases

---
+++ src/services/use-cases/addYogaExerciseToFavorites.js
@@ -0,0 +1,39 @@
+import User from "@/src/models/UserModel";
+
+const addYogaExerciseToFavorites = async ({ name, email, yogaId }) => {
+	const userExists = await User.find({ email: email }).exec();
+
+	if (userExists.length !== 0) {
+		/** User exists */
+		const [yogaObjectId] = userExists.map((user) => user.yoga);
+
+		const yoga = yogaObjectId.filter(
+			(objectId) => objectId.toString() === yogaId
+		);
+
+		if (yoga.length !== 1) {
+			/** add yoga exercise to user favorites */
+			userExists[0].yoga.push(yogaId);
+			userExists[0].save();
+		} else {
+			/** remove yoga exercise from user favorites */
+			const filteredYoga = yogaObjectId.filter(
+				(objectId) => objectId.toString() !== yogaId
+			);
+			userExists[0].yoga = filteredYoga;
+			userExists[0].save();
+		}
+	} else {
+		/** user does not exist */
+		await User.create({
+			name: {
+				firstname: name.split(" ")[0],
+				lastname: name.split(" ")[1],
+			},
+			email: email,
+			yoga: yogaId,
+		});
+	}
+};
+
+module.exports = { addYogaExerciseToFavorites };
