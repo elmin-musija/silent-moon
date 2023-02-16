16c8c639a4ef0d466f0ef4e2f573db4689b1cceb
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-16 15:27:14 +0100

    feat: add notification component

---
+++ components/notification/notification.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useContext } from "react";
+import NotificationContext from "../../context/context";
+import { motion, AnimatePresence } from "framer-motion";
+import styles from "./notification.module.css";
+
+const Notification = (props) => {
+	const {
+		hideNotification,
+		getShowNotification,
+		getNotificationType,
+		getMessage,
+	} = useContext(NotificationContext);
+
+	let notificationClass = "";
+
+	const onClickHandler = () => {
+		hideNotification();
+	};
+
+	if (getNotificationType() === "success") {
+		notificationClass = `${styles.notification} ${styles.success}`;
+	} else if (getNotificationType() === "warning") {
+		notificationClass = `${styles.notification} ${styles.warning}`;
+	} else {
+		notificationClass = `${styles.notification} ${styles.error}`;
+	}
+
+	return (
+		<AnimatePresence initial={false}>
+			{getShowNotification() && (
+				<motion.div
+					initial={{ y: "-100px", opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 1, type: "spring" }}
+					className={notificationClass}
+					onClick={onClickHandler}
+				>
+					<p className={styles.notificationText}>{getMessage()}</p>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
+};
+
+export default Notification;
