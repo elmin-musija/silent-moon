26425aabcc85d777ba57f0607ce5681c4391182b
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-16 11:09:32 +0100

    fix: handle case user does not already exist in database

--- src/services/use-cases/listAllFavoriteYogaExercises.js
+++ src/services/use-cases/listAllFavoriteYogaExercises.js
@@ -3,23 +3,28 @@ import User from "@/src/models/UserModel";
 
 const listAllFavoriteYogaExercises = async ({ email }) => {
 	await connectToDatabase();
-	const [result] = await User.find({ email: email }).populate("yoga").exec();
-	const { yoga } = result;
-
-	const allFavoriteYogaExercises = yoga.map((element) => ({
-		_id: element._id.toString(),
-		title: element.title,
-		description: element.description,
-		imageUrl: element.imageUrl,
-		category: element.category,
-		level: element.level,
-		duration: {
-			minutes: element.duration.minutes,
-			seconds: element.duration.seconds,
-		},
-		videoUrl: element.videoUrl,
-	}));
-	return allFavoriteYogaExercises;
+	const result = await User.find({ email: email }).populate("yoga").exec();
+	if (result.length !== 0) {
+		/** user does not exist */
+		const { yoga } = result[0];
+		const allFavoriteYogaExercises = yoga.map((element) => ({
+			_id: element._id.toString(),
+			title: element.title,
+			description: element.description,
+			imageUrl: element.imageUrl,
+			category: element.category,
+			level: element.level,
+			duration: {
+				minutes: element.duration.minutes,
+				seconds: element.duration.seconds,
+			},
+			videoUrl: element.videoUrl,
+		}));
+		return allFavoriteYogaExercises;
+	} else {
+		/** user does not exist */
+		return [];
+	}
 };
 
 module.exports = {
