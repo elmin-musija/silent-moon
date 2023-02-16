8071148f16e92922996113762de3d030f952ba03
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-16 09:52:13 +0100

    feat: add user controller to api endpoint of yoga exercises

--- pages/api/yoga/exercise.js
+++ pages/api/yoga/exercise.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { YogaController } from "@/src/controllers/index";
+import { UserController } from "@/src/controllers/index";
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
-		await YogaController.postInsertYogaExercise(req, res);
+		await UserController.postAddYogaExerciseToFavorites(req, res);
 		return;
 	}
 	res.status(400).json({ status: "Bad Request!" });
