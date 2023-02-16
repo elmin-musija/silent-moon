70fa3c7d0ef0e9c2c5eb268266a443fee63d6c67
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-16 12:55:21 +0100

    feat: add album cover to music page

--- pages/music/index.js
+++ pages/music/index.js
@@ -68,6 +68,13 @@ const MusicPage = ({ playlistInfo, playlistTracks }) => {
 	return (
 		<div className={styles.musicPage}>
 			<Title />
+			<Image
+				src={playlistInfo.images[0].url}
+				width="160"
+				height="160"
+				alt="album cover"
+				className={styles.albumCover}
+			/>
 			<Link
 				href={`/player/q?type=${playlistInfo.type}&offset=0&id=${playlistInfo.id}`}
 			>
