2d3e02b4a378a9818c5d0f3cfcf30f9fa7cc3c61
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-17 11:49:10 +0100

    feat: add remote patterns to next.config.js allowing for loading profile pictures from providers

--- next.config.js
+++ next.config.js
@@ -26,6 +26,24 @@ module.exports = {
 				port: "",
 				pathname: "**",
 			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				port: "",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				port: "",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.discordapp.com",
+				port: "",
+				pathname: "**",
+			},
 		],
 	},
 };
