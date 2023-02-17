f496f32144843079940d8f9971ebcac0de49dad7
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-17 13:08:28 +0100

    feat: add profile picture, profile name and logout button to profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,14 +1,36 @@
 import React, { useEffect, useRef, useState } from "react";
-import Title from "@/components/title/title";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { getServerSession } from "next-auth/next";
 import { NextAuthOptions } from "../api/auth/[...nextauth]";
+import { signOut } from "next-auth/react";
 import { UserService } from "@/src/services/use-cases/index";
 import { uid } from "uid";
 import Link from "next/link";
 import Image from "next/image";
+import Title from "@/components/title/title";
 import styles from "./profile.module.css";
 
 const ProfilePage = ({ allYogaFavorites }) => {
+	const { data: session } = useSession();
+	const router = useRouter();
+
+	const onLogoutHandler = async () => {
+		const redirect = await signOut({ redirect: false, callbackUrl: "/signin" });
+		router.push(redirect.url);
+	};
+
+	let image = "/img/user.svg";
+	let firstName = "";
+	if (session) {
+		const { user } = session;
+		const username = session.user.name;
+		firstName = username.split(" ")[0];
+		if (user.picture) {
+			image = user.picture;
+		}
+	}
+
 	const inputFieldSearchRef = useRef(null);
 	const [filteredYogaFavorites, setFilteredYogaFavorites] =
 		useState(allYogaFavorites);
@@ -56,88 +78,100 @@ const ProfilePage = ({ allYogaFavorites }) => {
 
 	return (
 		<div className={styles.profilePage}>
-			<Title />
+			<header>
+				<Title />
+			</header>
 
-			{/** Search bar */}
-			<div className={styles.searchbar}>
-				<form onChange={onInputSearchYogaHandler}>
-					<input
-						type="text"
-						name="input-yoga-search"
-						id="input-yoga-search"
-						ref={inputFieldSearchRef}
-						placeholder="Search for your favorites sessions or meditations"
-					/>
-				</form>
-				<div className={styles.searchIconContainer}>
-					<Image src="/img/search.svg" width="15" height="15" alt="search" />
+			<main>
+				{/** Profile image, name and logout */}
+				<div className={styles.profileInfoContainer}>
+					<Image src={image} width="80" height="80" alt="profile picture" />
+					<div>
+						<p>{firstName}</p>
+						<button onClick={onLogoutHandler}>Logout</button>
+					</div>
 				</div>
-			</div>
 
-			<h2>Favourite Yoga Session</h2>
-			<div className={styles.slider}>
-				{filteredYogaFavorites?.map((element) => (
-					<Link
-						key={element._id}
-						href={`/yoga/${element._id}`}
-						className={styles.sliderItem}
-					>
-						<div className={styles.imgageContainer} key={uid()}>
-							<Image
-								src={element.imageUrl}
-								width={150}
-								height={150}
-								alt={element.title}
-								key={element._id}
-								priority
-							></Image>
-						</div>
+				{/** Search bar */}
+				<div className={styles.searchbar}>
+					<form onChange={onInputSearchYogaHandler}>
+						<input
+							type="text"
+							name="input-yoga-search"
+							id="input-yoga-search"
+							ref={inputFieldSearchRef}
+							placeholder="Search for your favorites sessions or meditations"
+						/>
+					</form>
+					<div className={styles.searchIconContainer}>
+						<Image src="/img/search.svg" width="15" height="15" alt="search" />
+					</div>
+				</div>
 
-						<div key={uid()} className={styles.itemInfo}>
-							<h3>{element.title}</h3>
-							<div className={styles.itemSubInfo}>
-								<p key={uid()}>{element.level}</p>
-								<p key={uid()}>
-									{element.duration.minutes}:
-									{String(element.duration.seconds).padStart(2, "0")} min
-								</p>
+				<h2>Favourite Yoga Session</h2>
+				<div className={styles.slider}>
+					{filteredYogaFavorites?.map((element) => (
+						<Link
+							key={element._id}
+							href={`/yoga/${element._id}`}
+							className={styles.sliderItem}
+						>
+							<div className={styles.imgageContainer} key={uid()}>
+								<Image
+									src={element.imageUrl}
+									width={150}
+									height={150}
+									alt={element.title}
+									key={element._id}
+									priority
+								></Image>
 							</div>
-						</div>
-					</Link>
-				))}
-				{displayMessageFavoriteYoga()}
-			</div>
-			<h2>Favourite Meditations</h2>
-			{/* <div className={styles.slider}>
-				{allMeditationFavorites?.map((element) => (
-					<Link
-						key={element._id}
-						href={`/yoga/${element._id}`}
-						className={styles.sliderItem}
-					>
-						<div className={styles.imgageContainer} key={uid()}>
-							<Image
-								src={element.imageUrl}
-								width={150}
-								height={150}
-								alt={element.title}
-								key={element._id}
-								priority
-							></Image>
-						</div>
-
-						<div key={uid()} className={styles.itemInfo}>
-							<h3>{element.title}</h3>
-							<div className={styles.itemSubInfo}>
-								<p key={uid()}>{element.level}</p>
-								<p key={uid()}>
-									{element.duration.minutes}:{element.duration.seconds} min
-								</p>
+							<div key={uid()} className={styles.itemInfo}>
+								<h3>{element.title}</h3>
+								<div className={styles.itemSubInfo}>
+									<p key={uid()}>{element.level}</p>
+									<p key={uid()}>
+										{element.duration.minutes}:
+										{String(element.duration.seconds).padStart(2, "0")} min
+									</p>
+								</div>
 							</div>
-						</div>
-					</Link>
-				))}
-			</div> */}
+						</Link>
+					))}
+					{displayMessageFavoriteYoga()}
+				</div>
+
+				<h2>Favourite Meditations</h2>
+				{/* <div className={styles.slider}>
+					{allMeditationFavorites?.map((element) => (
+						<Link
+							key={element._id}
+							href={`/yoga/${element._id}`}
+							className={styles.sliderItem}
+						>
+							<div className={styles.imgageContainer} key={uid()}>
+								<Image
+									src={element.imageUrl}
+									width={150}
+									height={150}
+									alt={element.title}
+									key={element._id}
+									priority
+								></Image>
+							</div>
+							<div key={uid()} className={styles.itemInfo}>
+								<h3>{element.title}</h3>
+								<div className={styles.itemSubInfo}>
+									<p key={uid()}>{element.level}</p>
+									<p key={uid()}>
+										{element.duration.minutes}:{element.duration.seconds} min
+									</p>
+								</div>
+							</div>
+						</Link>
+					))}
+				</div> */}
+			</main>
 		</div>
 	);
 };
