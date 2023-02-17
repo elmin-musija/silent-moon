c8b2f9f56d4e5606b68de08843144bd65063eab3
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-02-17 12:14:25 +0100

    feat: add profile picture to title

--- components/title/title.js
+++ components/title/title.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./title.module.css";
 
 const Title = () => {
-	return <h1 className={styles.title}>silent moon</h1>;
+	const router = useRouter();
+	const { data: session } = useSession();
+
+	let image = "/img/user.svg";
+
+	if (session) {
+		const { user } = session;
+		if (user.picture) {
+			image = user.picture;
+		}
+	}
+
+	return (
+		<div className={styles.title}>
+			<h1>silent moon</h1>
+			{router.pathname !== "/profile" && (
+				<Link href="/profile">
+					<Image src={image} width="30" height="30" alt="profile" />
+				</Link>
+			)}
+		</div>
+	);
 };
 
 export default Title;
