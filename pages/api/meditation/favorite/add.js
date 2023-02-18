1549727f8cf9ab39b2ce5848166166ecc730de37
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-18 11:51:03 +0100

    feat: add api route add meditation course to user favorites

---
+++ pages/api/meditation/favorite/add.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { UserController } from "@/src/controllers/index";
+
+export default async function handler(req, res) {
+	if (req.method === "POST") {
+		await UserController.postAddMeditationCourseToFavorites(req, res);
+		return;
+	}
+	res.status(400).json({ status: "Bad Request!" });
+}
